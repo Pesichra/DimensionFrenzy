b038cce6df664b4ab207144df32d5d6183209092
Enis Kerem Çakmak <kerem.cakmak@ug.bilkent.edu.tr>
2024-12-02 15:40:35 +0300

    dimension change mechanic on walls, props, puzzle chest design

---
+++ Assets/Scripts/PropsScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropsScript : MonoBehaviour
+{
+    public int dimension;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameManager.OnDimensionChange += HandleDimensionChange;
+    }
+
+    void HandleDimensionChange(int newDimension)
+    {
+        if (newDimension == dimension)
+        {
+            gameObject.SetActive(true);
+        }else{
+            gameObject.SetActive(false);
+        }
+    }
+}
